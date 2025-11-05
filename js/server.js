288936be0c79f4532c62a7d322eab8b25509fc31
HampusAyalla <hampus.melinosterlindh@skola.taby.se>
2025-11-05 14:43:24 +0100

    Filstruktur och börja js

---
+++ js/server.js
@@ -0,0 +1,16 @@
+
+
+
+
+app.post("/login", async, (req, res) => {
+    const {username, password} = req.body;
+    const user = 
+
+
+});
+
+app.post("/posts", async, (req, res) => {
+    const {title, content, username} = req.body;
+    
+
+});
